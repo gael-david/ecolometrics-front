2c58cce541b2ff5505f11a774bd10fca503268a4
Gaël David <gaeldavid44@gmail.com>
2021-05-19 15:21:27 +0200

    feat: add coming-soon page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
+import Header from "../layouts/Header/Header";
+import Footer from "../layouts/Footer/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Ecolometrics - Open knowledge hub on ecological transitions</title>
+        <meta name="description" content="Open knowledge hub on ecological transitions" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&family=Roboto:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jgthms/minireset.css@master/minireset.min.css" />
+      </Head>
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
